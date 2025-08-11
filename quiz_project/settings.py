7c94eeb41594a96b059917b0e7a3a5c7bca66d8c
Kavya-N03 <nkavya649@gmail.com>
2025-08-11 21:30:27 +0530

    updated database

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,12 @@ SECRET_KEY = 'django-insecure-3%a$)pr63crw6o+cwijho9$%&#^qkqrzer1rr^hmcj*m)%!!fj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quiz-app-g4aw.onrender.com',
+                 'localhost',
+                 '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://quiz-app-g4aw.onrender.com'
+]
 
 
 # Application definition
@@ -77,12 +84,23 @@ WSGI_APPLICATION = 'quiz_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASE_URL = {
+        'default':dj_database_url.parse(DATABASE_URL,conn_max_age=600,ssl_require=True)
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
 
 
 # Password validation
